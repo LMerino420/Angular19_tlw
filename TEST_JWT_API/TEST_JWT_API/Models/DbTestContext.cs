42b2d5f367e36a0b040d9e8c8c1d769a1756d767
LMerino420 <merinolev96@gmail.com>
2025-01-28 08:39:42 -0600

    Config
    °DbContext
    °JWT

---
+++ TEST_JWT_API/TEST_JWT_API/Models/DbTestContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TEST_JWT_API.Models;
+
+public partial class DbTestContext : DbContext
+{
+    public DbTestContext()
+    {
+    }
+
+    public DbTestContext(DbContextOptions<DbTestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("producto_pk");
+
+            entity.ToTable("producto");
+
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("marca");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Precio)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("precio");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUser).HasName("usuario_pk");
+
+            entity.ToTable("usuario");
+
+            entity.Property(e => e.IdUser).HasColumnName("idUser");
+            entity.Property(e => e.Clave)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("clave");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("correo");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TEST_JWT_API/TEST_JWT_API/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TEST_JWT_API.Models;
+
+public partial class Usuario
+{
+    public int IdUser { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Correo { get; set; }
+
+    public string? Clave { get; set; }
+}
